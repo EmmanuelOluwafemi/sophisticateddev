f47d4ae8f0053bd96d0dc7fc43039b00fdc46116
EmmanuelOluwafemi <Seek9ja@gmail.com>
2021-06-15 17:31:17 +0100

    added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import Hero from '../components/Hero';
 import Styled from 'styled-components';
 import Marquee from '../components/Marquee';
 import FeaturedSlideMobile from '../components/featuredSlides/FeaturedSlideMobile';
+import { Helmet } from "react-helmet";
+
+import favicon from '../assets/images/faviconBg.jpg';
 
 const Home = () => {
 
@@ -11,6 +14,29 @@ const Home = () => {
     
     return (
         <>
+            <Helmet>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta charSet="utf-8" />
+                <meta name="description" content='I am Iheonye Chukwuemeka Kingsley, a product designer passionate about building 
+                    Human Centered designs for individuals and businesses.'></meta>
+        
+                <meta property="og:title" content='Sophisticateddev' key="ogtitle" />
+                <meta property="og:description" content='I am Iheonye Chukwuemeka Kingsley, a product designer passionate about building 
+                    Human Centered designs for individuals and businesses.' key="ogdesc" />
+                <title>Sophisticateddev</title>
+                {/* Twitter */}
+                <meta name="twitter:card" content="I am Iheonye Chukwuemeka Kingsley, a product designer passionate about building 
+                    Human Centered designs for individuals and businesses." key="twcard" />
+                <meta name="twitter:creator" content='' key="twhandle" />
+
+                {/* Open Graph */}
+                <meta property="og:url" content='http://sophisticateddev.com' key="ogurl" />
+                <meta property="og:image" content={favicon} key="ogimage" />
+                <meta property="og:site_name" content='Sophisticateddev' key="ogsitename" />
+                <meta property="og:title" content='Sophisticateddev' key="ogtitle" />
+                <meta property="og:description" content='I am Iheonye Chukwuemeka Kingsley, a product designer passionate about building 
+                    Human Centered designs for individuals and businesses.' key="ogdesc" />
+            </Helmet>
             <Hero />
             <Featured>
                 <h1>Featured Projects</h1>
