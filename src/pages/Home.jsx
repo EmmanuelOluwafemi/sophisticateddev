246cc91e9a3491e0588b0708ff5b6e6581173677
EmmanuelOluwafemi <Seek9ja@gmail.com>
2021-06-18 23:01:28 +0100

    added updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,38 @@ import Styled from 'styled-components';
 import Marquee from '../components/Marquee';
 import FeaturedSlideMobile from '../components/featuredSlides/FeaturedSlideMobile';
 
+import Helmet from 'react-helmet';
+
+import logo from '../../public/faviconbig.jpg';
+
 const Home = () => {
 
     // window.scrollTo(0, 0);
+
+    <Helmet>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Sophisticateddev</title>
+        <meta name="title" content="Sophisticateddev"/>
+        <meta name="description" content="I am Iheonye Chukwuemeka Kingsley, a product designer passionate about building 
+                    Human Centered designs for individuals and businesses."/>
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://www.sophisticateddev.com/"/>
+        <meta property="og:title" content="Sophisticateddev"/>
+        <meta property="og:description" content="I am Iheonye Chukwuemeka Kingsley, a product designer passionate about building 
+                    Human Centered designs for individuals and businesses."/>
+        <meta property="og:image" content={logo}/>
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://www.sophisticateddev.com/"/>
+        <meta property="twitter:title" content="Sophisticateddev"/>
+        <meta property="twitter:description" content="I am Iheonye Chukwuemeka Kingsley, a product designer passionate about building 
+                    Human Centered designs for individuals and businesses."/>
+        <meta property="twitter:image" content={logo}/>
+
+    </Helmet>
     
     return (
         <>
